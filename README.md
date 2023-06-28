7d383ef2ce06efc55b01a572dbade5b88ae57f3c
nvhungw <119866957+nvhungw@users.noreply.github.com>
2023-06-28 21:51:51 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ES6-part2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-3knqxk)
---
+++ script.js
@@ -0,0 +1,27 @@
+// .map: ánh xạ từng thành phần của mảng cũ sang mảng mới
+// const images = [
+//   { height: '34px', width: '39px' },
+//   { height: '35px', width: '35px' },
+//   { height: '36px', width: '31px' },
+// ];
+// const result = images.map(item => {
+//   return {height: item.height}
+// })
+// console.log(result)
+////////////////////////////////////////////
+
+// fillter: Lọc giá trị cần thiết
+// const a = [1,2,3,4]
+// const result = a.filter(item => {
+//   return item % 2 !== 0
+// })
+// console.log(result)
+///////////////////////////////////////////
+
+// //reduce (số 0 là khởi tạo)
+// const a = [1, 2, 3, 4];
+// const resutlReduce = a.reduce((result, item) => result + item, 0);
+// console.log(resutlReduce);
+
+
+
